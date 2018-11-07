11e3d1a39cc72592023aa58673b08b2aadbfd3c8
Rye-Guy <ryanracioppo95@gmail.com>
2018-11-07 18:56:07 -0500

    installed redux-form and added a basic form

--- src/components/new_post.js
+++ src/components/new_post.js
@@ -1,13 +1,35 @@
 import React, { Component } from 'react';
+import { Field, reduxForm } from 'redux-form';
 
 class newPost extends Component {
+    renderField(field){
+        return (
+            <div className='form-group'>
+            <label>{field.label}</label>
+                <input className='form-control' type='text' {...field.input} />
+            </div>
+        )
+    }
+    renderTagsField(field){
+        return (
+            <div className='form-group'>
+            <label>Categories</label>
+                <input className='form-control' type='text' {...field.input} />
+            </div>
+        )
+    }
+
     render(){
         return(
-            <div>
-                New Post!
-            </div>
+            <form>
+                <Field name='title' label={'Title'} component={this.renderField} />
+                <Field name='tags'  label={'Categories'} component={this.renderField} />
+                <Field name='content'  label={'Post Body'} component={this.renderField} />
+            </form>
         );
     }
 }
 
-export default newPost
+export default reduxForm({
+    form: 'PostsNewForm'
+})(newPost)
