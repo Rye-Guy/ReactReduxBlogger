ae9a9f5b02c2163107ead0cb19316a8b61f344a0
Rye-Guy <ryanracioppo95@gmail.com>
2018-11-13 18:08:29 -0500

    added form vaildation

--- src/components/new_post.js
+++ src/components/new_post.js
@@ -7,6 +7,7 @@ class newPost extends Component {
             <div className='form-group'>
             <label>{field.label}</label>
                 <input className='form-control' type='text' {...field.input} />
+                {field.meta.touched ? field.meta.error : ''}
             </div>
         )
     }
@@ -19,17 +20,43 @@ class newPost extends Component {
         )
     }
 
+    onSubmit(values){
+        console.log(values)
+    }
+
     render(){
+        const { handleSubmit } = this.props;
         return(
-            <form>
+            <form onSubmit={this.onSubmit.bind(this)}>
                 <Field name='title' label={'Title'} component={this.renderField} />
-                <Field name='tags'  label={'Categories'} component={this.renderField} />
+                <Field name='categories'  label={'Categories'} component={this.renderField} />
                 <Field name='content'  label={'Post Body'} component={this.renderField} />
+                <button className="btn btn-primary" type="submit">Submit</button>
             </form>
         );
     }
 }
 
+function validate(values){
+    const errors = {};
+
+    //validate inputs from values
+    if(!values.title){
+        errors.title = "Enter a title!";
+    }
+
+    if(!values.categories){
+        errors.categories = "Please select a category";
+    }
+
+    if(!values.content){
+        errors.content = 'Enter some damn info for your post... Fool!'
+    }
+    //we are checking to see if errors has any properties to it and if it does REDUX will not send the form 
+    return errors; 
+}
+
 export default reduxForm({
-    form: 'PostsNewForm'
+    form: 'PostsNewForm',
+    validate: validate
 })(newPost)
